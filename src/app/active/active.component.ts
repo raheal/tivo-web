953cba4052b4abd58273b675eeda12c980cff0aa
raheal <raheal.mazumder@gmail.com>
2020-03-06 11:20:24 -0800

    added stuff

--- src/app/active/active.component.ts
+++ src/app/active/active.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveComponent implements OnInit {
 
+
+  private diskSpaceMetrics : Object;
+
+  private numberOfFilesInStorage : Object;
+
   constructor() { }
 
+
+
   ngOnInit() {
+    this.diskSpaceMetrics = { total : 100, used : 50, free : 50};
   }
 
 }

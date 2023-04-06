2258bdb8ce4538f48636500a6b9ce76b51390788
raheal <raheal.mazumder@gmail.com>
2023-04-06 01:07:19 +0100

    updated the local server bat file

---
+++ run-local-server.bat
@@ -0,0 +1,7 @@
+@echo off
+rem this assumes that you have node 15.0.1 installed, alongside already running the command: "npm install --save express"
+rem "ng build" needs to be run, to put the transpiled javascript into the dist/tivo-download-service-web folder.
+
+rem ng build --prod
+
+node server.js
